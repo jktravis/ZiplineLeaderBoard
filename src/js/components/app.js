2169af72e1ab7209f581c014927d81dc90941c72
jktravis <joshua.travis@gmail.com>
2016-04-12 09:27:25 -0400

    Init

---
+++ src/js/components/app.js
@@ -0,0 +1,14 @@
+"use strict";
+var React = require('react');
+
+var App = React.createClass({
+  render: function render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = App;
